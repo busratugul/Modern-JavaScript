8e92f2c4d305560489bca57bebb8fdbb2fae2c60
Büşra Tuğul <134439412+busratugul@users.noreply.github.com>
2023-08-06 23:13:16 +0300

    Modern-JavaScript

--- Web Browser & APIs/Show Location App/script.js
+++ Web Browser & APIs/Show Location App/script.js
@@ -1,5 +1,6 @@
-//konumumuzu harita üzerinde göstermek
+//konumumuzu harita üzerinde göstermek 
 
+//burayı siteden copy paste yaparak aslında bir map başlatıyoruz
 const map = L.map('map').setView([0, 0], 2);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,6 +8,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
+
+//marker ile konumumuzu göstermek buradaki metodlar kutuphanenin bir parçası jsnin değil
 const marker = L.marker([0, 0]).addTo(map);
 
 navigator.geolocation.getCurrentPosition(function (pos) {
@@ -14,7 +17,8 @@ navigator.geolocation.getCurrentPosition(function (pos) {
   const lng = pos.coords.longitude;
 
   marker.setLatLng([lat, lng]).update();
-  map.setView([lat, lng], 13);
+  map.setView([lat, lng], 13); //bu sayı zoomdur
 
+  //buna popup eklemek istersek
   marker.bindPopup('<strong>Hello World</strong> <br> This is my location');
 });
